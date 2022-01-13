ab01d3828c776f492fba3efa93ee6d0b74863d8f
mingg123 <spgb123spgb@gmail.com>
2022-01-13 14:07:39 +0900

    feat : add API  addMovie, deleteMovie

--- MOVIEQL/grapql/db.js
+++ MOVIEQL/grapql/db.js
@@ -1,43 +1,60 @@
-export const people = [
+export let movies = [
   {
     id: 0,
-    name: "minji",
-    age: 18,
-    gender: "male",
+    name: "특송",
+    score: 9,
   },
   {
     id: 1,
-    name: "Daal",
-    age: 18,
-    gender: "male",
+    name: "스파이더맨:노웨이홈",
+    score: 8,
   },
   {
     id: 2,
-    name: "JD",
-    age: 18,
-    gender: "male",
+    name: "웨스트사이드스토리",
+    score: 7,
   },
   {
     id: 3,
-    name: "moodaddi",
-    age: 18,
-    gender: "male",
+    name: "경관의 피",
+
+    score: 6,
   },
   {
     id: 4,
-    name: "flynn",
-    age: 18,
-    gender: "male",
+    name: "구찌",
+
+    score: 5,
   },
   {
     id: 5,
-    name: "minji",
-    age: 18,
-    gender: "male",
+    name: "킹스맨",
+    score: 4,
   },
 ];
 
+export const getMovies = () => movies;
 export const getById = (id) => {
-  const filterdPeople = people.filter((person) => id === person.id);
-  return filterdPeople[0];
+  const filteredMovies = movies.filter((movie) => id === movie.id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovie = movies.filter((movie) => movie.id != id);
+  if (movies.length > cleanedMovie.length) {
+    movies = cleanedMovie;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
